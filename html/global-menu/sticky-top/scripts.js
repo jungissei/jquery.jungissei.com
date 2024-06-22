18b6f4a861085c48a8ada8f0d6eb0245ebf0e0c0
jungiIssei <jungissei@gmail.com>
2024-06-22 21:31:19 +0900

    ヘッダー横スクロール対応をPCのみ対応 #40

--- html/global-menu/sticky-top/scripts.js
+++ html/global-menu/sticky-top/scripts.js
@@ -74,14 +74,16 @@ $(window).on('load', function() {
 // --------------------------------------
 $(window).on('scroll', function(){
 
-  let $header_inner = $('#template_header > .layout_inner'); // position:fixed; が付与されている要素を設定
+  if (window.matchMedia( '(min-width: 768px)' ).matches) {
+    let $header_inner = $('#template_header > .layout_inner'); // position:fixed; が付与されている要素を設定
 
-  if($header_inner.length > 0 && $header_inner.css('position') === 'fixed'){
+    if($header_inner.length > 0 && $header_inner.css('position') === 'fixed'){
 
-    let window_scroll_left = parseInt($(window).scrollLeft());
-    let left_value = (window_scroll_left == 0) ? '' : -window_scroll_left;
+      let window_scroll_left = parseInt($(window).scrollLeft());
+      let left_value = (window_scroll_left == 0) ? '' : -window_scroll_left;
 
-    // 横スクロールに対応するため、leftプロパティを調整
-    $header_inner.css('left', left_value);
+      // 横スクロールに対応するため、leftプロパティを調整
+      $header_inner.css('left', left_value);
+    }
   }
 });
