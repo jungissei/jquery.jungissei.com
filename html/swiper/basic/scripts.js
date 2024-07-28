8f9c8cc4c994e48ef26dfdaa386cc7e269198984
Jung Issei <isseeeei@gmail.com>
2024-07-28 22:55:35 +0900

    【Swiper】デフォルト : アクセシビリティ対応#53

--- html/swiper/basic/scripts.js
+++ html/swiper/basic/scripts.js
@@ -11,6 +11,18 @@ $(function(){
     navigation: {
       nextEl: swiper_block + '.swiper-button-next',
       prevEl: swiper_block + '.swiper-button-prev'
+    },
+    // キーボード操作
+    keyboard: {
+      enabled: true,
+    },
+    // aria-labelの内容を変更
+    a11y: {
+      containerMessage:'デモのカルーセル',
+      prevSlideMessage: '前のスライドへ',
+      nextSlideMessage: '次のスライドへ',
+      slideLabelMessage:'{{index}}枚目のスライド',
+      paginationBulletMessage: '{{index}}枚目のスライドを表示'
     }
   });
 
