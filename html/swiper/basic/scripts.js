89cee97dc1e7c5a4f9e468f16f1a2d1ab92b7904
Jung Issei <isseeeei@gmail.com>
2024-08-03 17:00:45 +0900

    スライダー基本系のリファクタリング

--- html/swiper/basic/scripts.js
+++ html/swiper/basic/scripts.js
@@ -1,27 +1,27 @@
 $(function(){
 
   let swiper_block     = '.swiper-block';     // スライダー全体を囲む要素
-  let swiper_container = '.swiper-container'; // スライダーアイテムを囲む要素
+  let swiper_container = swiper_block + ' ' + '.swiper-container'; // スライダーアイテムを囲む要素
 
   // スライダーアイテムが存在しない場合は処理を中断
-  if($(swiper_block + ' ' + swiper_container).length === 0){
+  if($(swiper_container).length === 0){
     return;
   }
 
 
-  let swiper_pagination  = '.swiper-pagination';  // ページネーション要素
-  let swiper_button_next = '.swiper-button-next'; // 次のスライドボタン要素
-  let swiper_button_prev = '.swiper-button-prev'; // 前のスライドボタン要素
+  let swiper_pagination  = swiper_block + ' ' + '.swiper-pagination';  // ページネーション要素
+  let swiper_button_next = swiper_block + ' ' + '.swiper-button-next'; // 次のスライドボタン要素
+  let swiper_button_prev = swiper_block + ' ' + '.swiper-button-prev'; // 前のスライドボタン要素
 
-  new Swiper(swiper_block + ' ' + swiper_container, {
+  new Swiper(swiper_container, {
     effect: 'fade',
     pagination: {
-      el: swiper_block + ' ' + swiper_pagination,
+      el: swiper_pagination,
       clickable: true
     },
     navigation: {
-      nextEl: swiper_block + ' ' + swiper_button_next,
-      prevEl: swiper_block + ' ' + swiper_button_prev
+      nextEl: swiper_button_next,
+      prevEl: swiper_button_prev
     },
     // キーボード操作
     keyboard: {
