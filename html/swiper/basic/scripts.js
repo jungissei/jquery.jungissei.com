8e62caf8d6544c57263fb098b9d8edda09eced00
Jung Issei <isseeeei@gmail.com>
2024-08-03 15:54:55 +0900

    スライダー基本系のリファクタリング

--- html/swiper/basic/scripts.js
+++ html/swiper/basic/scripts.js
@@ -1,16 +1,27 @@
-
 $(function(){
-  let swiper_block = '.swiper-block ';
 
-  new Swiper(swiper_block + '.swiper-container', {
+  let swiper_block     = '.swiper-block';     // スライダー全体を囲む要素
+  let swiper_container = '.swiper-container'; // スライダーアイテムを囲む要素
+
+  // スライダーアイテムが存在しない場合は処理を中断
+  if($(swiper_block + ' ' + swiper_container).length === 0){
+    return;
+  }
+
+
+  let swiper_pagination  = '.swiper-pagination';  // ページネーション要素
+  let swiper_button_next = '.swiper-button-next'; // 次のスライドボタン要素
+  let swiper_button_prev = '.swiper-button-prev'; // 前のスライドボタン要素
+
+  new Swiper(swiper_block + ' ' + swiper_container, {
     effect: 'fade',
     pagination: {
-      el: swiper_block + '.swiper-pagination',
+      el: swiper_block + ' ' + swiper_pagination,
       clickable: true
     },
     navigation: {
-      nextEl: swiper_block + '.swiper-button-next',
-      prevEl: swiper_block + '.swiper-button-prev'
+      nextEl: swiper_block + ' ' + swiper_button_next,
+      prevEl: swiper_block + ' ' + swiper_button_prev
     },
     // キーボード操作
     keyboard: {
@@ -25,5 +36,4 @@ $(function(){
       paginationBulletMessage: '{{index}}枚目のスライドを表示'
     }
   });
-
 });
